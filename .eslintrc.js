afbf44efe0220407d2f77d29aca34cee1609370f
Vukasin Skondric <vukasin.skondric@gmail.com>
2022-01-28 00:45:58 +0100

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    'no-multi-spaces': ['error']
+  },
+  plugins: [
+    'html'
+  ]
+};
